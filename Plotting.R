3bb583dcb1e3fa9d1cc4a4c06b7f8e177dda4c5a
Misha Tseitliani <12852413+MishaTs@users.noreply.github.com>
2024-03-28 20:30:02 +0000

    more plotting (of weather data)

--- Plotting.R
+++ Plotting.R
@@ -3,28 +3,188 @@ library(ggplot2)
 library(viridisLite)
 
 data <- data.frame(
-  Design = factor(rep(c("Compact", "Diffuse"), each = 3), levels = c("Compact", "Diffuse")),
-  Coefficient = rep(c("Sigma", "Density", "Detection"), times = 2),
+  Design = factor(rep(c("Compact", "Diffuse"), each = 3), 
+                  levels = c("Compact", "Diffuse")),
+  Coefficient = rep(c("Sigma (km)", "Density (per 100 km2)", "Detection"), times = 2),
   Estimate = c(12.23, 0.12, 0.023, 3.27, 0.534, 0.023),
   CI_lower = c(8.78, 0.041, 0.013, 2.45, 0.256, 0.013),
   CI_upper = c(17.04, 0.316, 0.040, 4.35, 1.113, 0.040),
-  Session = factor(rep(c("0", "1"), each = 3))
+  Session = factor(rep(c("1", "2"), each = 3))
 )
 
-plot <- ggplot(data, aes(x = Session, y = Estimate, color = Design)) +
-  geom_point(position = position_dodge(width = 0.25), size = 3) + # thicker points for estimates
-  geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper), width = 0.25, position = position_dodge(width = 0.25), size = 1.5) +  # thicker error bars
-  facet_wrap(~Coefficient, scales = "free_y") +
+#data$Coefficient <- factor(data$Coefficient, levels=c("A", "B", "C"))
+
+#lbs <- setNames(c("'Sigma (km)'", 
+#                  "'Density (per 100' (km*2) ')", 
+#                  "'Detection'"),
+#               LETTERS[1:3]
+#)[levels(data$Coefficient)]
+
+plot <- ggplot(data, aes(x = Session, y = Estimate, fill = Design, colour = Design)) +
+  geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper), 
+                width = 0.2, 
+                position = position_dodge(width = 0.25), 
+                size = 1) +  # thicker error bars
+  scale_colour_viridis_d(begin = 0.2,
+                         end = 0.8,
+                         direction = -1) +
+  geom_point(position = position_dodge(width = 0.25), 
+             size = 3, 
+             colour="black", 
+             pch = 21) + # thicker points for estimates
+  facet_wrap(~Coefficient, 
+             #labeller=as_labeller(lbs, label_parsed),
+             scales = "free_y") +
   theme_bw() +
-  labs(x = "", y = "Estimate") +
-  scale_color_manual(values = c("Compact" = "darkgreen", "Diffuse" = "darkblue")) +
+  labs(x = "Session", y = "Estimate") +
+  scale_fill_viridis_d(begin = 0.2,
+                        end = 0.8,
+                        direction = -1) +
   scale_x_discrete(labels = c("0", "1")) +  # Set specific labels for the x axis.
   theme(
-    axis.text.x = element_text(size = 12),  # Adjust x-axis text size
-    axis.text.y = element_text(size = 12),  # Adjust y-axis text size
-    axis.title.y = element_text(size = 14),  # Adjust y-axis label size
+    axis.text = element_text(size = 12),  # Adjust axis text size
+    axis.title = element_text(size = 14),  # Adjust axis label size
     strip.text = element_text(size = 14)  # Adjust facet label size
   )
 
 # Save the plot with transparent background
-ggsave("plot.png", plot, bg = "transparent")
+ggsave("CoefPlot.png", 
+       plot, 
+       bg = "transparent", 
+       dpi = 300, 
+       width = 30, 
+       height = 10, 
+       units = "cm")
+
+
+
+library(openmeteo)
+geocode("Gulmit")
+geocode("Nagir")
+
+#looking at Hispar Village which is in both surveys
+lat <- 217/6
+lon <- 75
+dates1 <- seq(as.Date("2016-03-01"), as.Date("2016-05-31"), by="days")
+dates2 <- seq(as.Date("2018-04-01"), as.Date("2018-06-30"), by="days")
+
+weatherHistData1 <- weather_history(location = c(lat,lon),
+                                    start = min(dates1),
+                                    end = max(dates1),
+                                    daily = c("temperature_2m_max",
+                                              "temperature_2m_min",
+                                              "temperature_2m_mean",
+                                              "apparent_temperature_max",
+                                              "apparent_temperature_min",
+                                              "apparent_temperature_mean",
+                                              "sunrise",
+                                              "sunset",
+                                              "daylight_duration",
+                                              "sunshine_duration",
+                                              "precipitation_sum",
+                                              "rain_sum",
+                                              "snowfall_sum",
+                                              "precipitation_hours",
+                                              "wind_speed_10m_max",
+                                              "wind_gusts_10m_max",
+                                              "wind_direction_10m_dominant",
+                                              "shortwave_radiation_sum",
+                                              "et0_fao_evapotranspiration"))
+
+weatherHistData2 <- weather_history(location = c(lat,lon),
+                                    start = min(dates2),
+                                    end = max(dates2),
+                                    daily = c("temperature_2m_max",
+                                              "temperature_2m_min",
+                                              "temperature_2m_mean",
+                                              "apparent_temperature_max",
+                                              "apparent_temperature_min",
+                                              "apparent_temperature_mean",
+                                              "sunrise",
+                                              "sunset",
+                                              "daylight_duration",
+                                              "sunshine_duration",
+                                              "precipitation_sum",
+                                              "rain_sum",
+                                              "snowfall_sum",
+                                              "precipitation_hours",
+                                              "wind_speed_10m_max",
+                                              "wind_gusts_10m_max",
+                                              "wind_direction_10m_dominant",
+                                              "shortwave_radiation_sum",
+                                              "et0_fao_evapotranspiration"))
+
+weatherHistDat1 <- weatherHistData1 %>% rownames_to_column("i") %>% mutate(Session = 1,
+                                                                            id = as.numeric(i),
+                                                                            day = as.Date(format(date, format="%m-%d"),
+                                                                                          format = "%m-%d"))
+weatherHistDat2 <- weatherHistData2 %>% rownames_to_column("i") %>% mutate(Session = 2,
+                                                                            id = as.numeric(i) + 31,
+                                                                            day = as.Date(format(date, format="%m-%d"),
+                                                                                          format = "%m-%d"))
+weatherDat <- rbind(weatherHistData1, weatherHistData2)
+weatherDatAlt <- left_join(weatherHistData1, weatherHistData2, by = "id")
+
+ggplot() + 
+  geom_ribbon(data = weatherHistDat1, 
+              aes(x=day, y=daily_temperature_2m_mean, 
+                  ymin=daily_temperature_2m_min, 
+                  ymax=daily_temperature_2m_max),
+              fill="cyan", alpha = 0.25) +
+  geom_line(data = weatherHistDat1, 
+            aes(x=day, y=daily_temperature_2m_mean), linewidth=1, color="blue") + 
+  geom_ribbon(data = weatherHistDat2, 
+              aes(x=day, y=daily_temperature_2m_mean, 
+                  ymin=daily_temperature_2m_min, 
+                  ymax=daily_temperature_2m_max),
+              fill="pink", alpha = 0.25) +
+  geom_line(data = weatherHistDat2, 
+    aes(x=day, y=daily_temperature_2m_mean), linewidth=1, color="red") + 
+  xlab("Survey Date") + 
+  ylab("Temperature (C)") + 
+  theme_bw()
+
+#sunshine duration?
+ggplot() + 
+  geom_line(data = weatherHistDat1, 
+            aes(x=day, y=daily_sunshine_duration), linewidth=1, color="blue") + 
+  geom_line(data = weatherHistDat2, 
+            aes(x=day, y=daily_sunshine_duration), linewidth=1, color="red") + 
+  xlab("Survey Date") + 
+  ylab("Daily Snowfall Sum (mm)") + 
+  theme_bw()
+
+
+
+
+
+# apparent temperature is better
+ggplot() + 
+  geom_ribbon(data = weatherHistDat1, 
+              aes(x=day, y=daily_apparent_temperature_mean, 
+                  ymin=daily_apparent_temperature_min, 
+                  ymax=daily_apparent_temperature_max),
+              fill="cyan", alpha = 0.25) +
+  geom_line(data = weatherHistDat1, 
+            aes(x=day, y=daily_apparent_temperature_mean), linewidth=1, color="blue") + 
+  geom_ribbon(data = weatherHistDat2, 
+              aes(x=day, y=daily_apparent_temperature_mean, 
+                  ymin=daily_apparent_temperature_min, 
+                  ymax=daily_apparent_temperature_max),
+              fill="pink", alpha = 0.25) +
+  geom_line(data = weatherHistDat2, 
+            aes(x=day, y=daily_apparent_temperature_mean), linewidth=1, color="red") + 
+  xlab("Survey Date") + 
+  ylab("Apparent Temperature (C)") + 
+  theme_bw()
+
+# snowfall?
+ggplot() + 
+  geom_line(data = weatherHistDat1, 
+            aes(x=day, y=daily_snowfall_sum), linewidth=1, color="blue") + 
+  geom_line(data = weatherHistDat2, 
+            aes(x=day, y=daily_snowfall_sum), linewidth=1, color="red") + 
+  xlab("Survey Date") + 
+  ylab("Daily Snowfall Sum (mm)") + 
+  theme_bw()
+
